0ddf863f0852b484207b1445bf08c854fc149f94
radomir-k <radomir.kartas@gmail.com>
2021-02-26 02:36:30 +0200

    90%...

--- lesson_24/js/code.js
+++ lesson_24/js/code.js
@@ -17,7 +17,8 @@ function nArea(a, b) {
     return a*b;
 }
 
-function nPerfect(a) {
+// --- This code outputs some valid values ​​and hangs ---
+function checkPerfect(a) {
     let flag;
     let sum = 0;
     for (let i = 1; i < a; i++) {
@@ -29,7 +30,44 @@ function nPerfect(a) {
 }
 
 function perfectScan(a, b) {
-    for (i = a; i <= b; i++) {
-        if (nPerfect(i) == true) { console.log(BigInt(i)); }
+    for (let i = a; i <= b; i++) {
+        if (checkPerfect(i) == true) { console.log(i); }
     }
+}
+// ------------------------------------------------------
+
+function setTime(h, m, s) {
+
+    if (h < 10) {h = 0 + "" + h;}
+    if (m < 10) {m = 0 + "" + m;}
+    if (s < 10) {s = 0 + "" + s;}
+
+    let t = h +":"+ m + ":" + s;
+    return t;
+}
+
+function parsSec(h, m, s) {
+    let t = h*3600 + m*60 + s;
+    return t;
+}
+
+function parsTime(s) {
+
+    h = Nod(s, 3600);
+    m = Nod((s - h*3600), 60);
+    s = (s - h*3600 - m*60);
+
+    t = setTime(h, m, s);
+
+    return t;
+}
+
+function name(params) {
+    
+}
+
+// --- NOD ---
+function Nod(a, b) {
+    c = (a - a % b) / b;
+    return c;
 }

5715a30440af3ff651681a961c17a2aa7a0d861b
radomir-k <radomir.kartas@gmail.com>
2021-03-07 09:20:46 +0200

    lesson_28 v.01

--- lesson_27/js/code.js
+++ lesson_27/js/code.js
@@ -2,39 +2,81 @@
 // --- Task 1 ---
 
 class Circle {
+
     constructor(radius) { this.radius = radius; }
 
-    circleLength() { return 2 * Math.PI * this.radius; }
-    circleArea() { return Math.PI * (this.radius ** 2); }
+    circleLength() {
+
+        return 2 * Math.PI * this.radius;
+    }
+
+    circleArea() {
+
+        return Math.PI * (this.radius ** 2);
+    }
 
     set radius(value) {
-        if (value <= 0) { alert('Incorrect value entered!'); return; }
+        if (value <= 0) {
+
+            alert('Incorrect value entered!');
+
+            return;
+        }
         this._radius = value;
     }
 
-    get radius() { return this._radius; }
-    get diametr() { return this._radius * 2; }
+    get radius() {
+
+        return this._radius;
+    }
+
+    get diametr() {
+
+        return this._radius * 2;
+    }
 
 }
 
 class waterMarker {
+
     constructor(color, filling, consumption) {
+
         this.color = color;             // Using Color Ink
+
         this.filling = filling;         // Filling Percent
+
         this.consumption = consumption; // Consumption on Symbol
     }
 
-    set color(value) { this._color = value; }
+    set color(value) {
+
+        this._color = value;
+    }
 
     set filling(value) {
-        if ((value + this.filling) > 100) { alert('Too Much Ink!'); return; }
+
+        if ((value + this.filling) > 100) {
+
+            alert('Too Much Ink!'); return;
+        }
+
         this._filling = value;
     }
 
-    get color() { return this._color; }
-    get filling() { return this._filling; }
+    get color() {
+
+        return this._color;
+    }
+
+    get filling() {
+
+        return this._filling;
+    }
 
     // --- Ink Consumption Calculation ---
-    inkConsumption(str, con) { return (str.length - str.split(' ').length -2) * con; }
+    inkConsumption(str, con) {
+
+        return (str.length - str.split(' ').length -2) * con;
+    }
 
 }
---
+++ lesson_28/js/code.js
@@ -0,0 +1,137 @@
+
+// --- Task 1 ---
+
+let playList = [
+    {
+    author: "LED ZEPPELIN",
+    song:   "STAIRWAY TO HEAVEN",
+    path:   "/lesson_28/mp3/LedZeppelin-StairwayToHeaven.mp3",
+    },
+
+    {
+    author: "QUEEN",
+    song:   "BOHEMIAN RHAPSODY",
+    path:   "/mp3/Queen-BohemianRhapsody.mp3",
+    },
+
+    {
+    author: "LYNYRD SKYNYRD",
+    song:   "FREE BIRD",
+    path:   "/mp3/LynyrdSkynyrd-FreeBird.mp3",
+    },
+
+    {
+    author: "DEEP PURPLE",
+    song:   "SMOKE ON THE WATER",
+    path:   "/mp3/DeepPurple-SmokeOnTheWater.mp3",
+    },
+
+    {
+    author: "JIMI HENDRIX",
+    song:   "ALL ALONG THE WATCHTOWER",
+    path:   "/mp3/JimiHendrix-AllAlongTheWatchtower.mp3",
+    },
+
+    {
+    author: "AC/DC",
+    song:   "BACK IN BLACK",
+    path:   "/mp3/AcDc-BackIinBlack.mp3",
+    },
+
+    {
+    author: "QUEEN",
+    song:   "WE WILL ROCK YOU",
+    path:   "/mp3/Queen-WeWillRockYou.mp3",
+    },
+
+    {
+    author: "METALLICA",
+    song:   "ENTER SANDMAN",
+    path:   "/mp3/Metallica-EnterSandman.mp3",
+    }
+];
+
+let boxSongs = document.createElement('div');
+    boxSongs.className = 'box-songs';
+    document.body.appendChild(boxSongs);
+
+let headerSongs = document.createElement('div');
+    headerSongs.className = 'header-songs';
+    headerSongs.innerHTML = 'PLAY LIST';
+    boxSongs.append(headerSongs);
+
+let songUl = document.createElement('ul');
+    songUl.className = 'list-songs';
+    boxSongs.append(songUl);
+
+    for (let index = 0; index < playList.length; ++index) {
+
+        let currentAuthor = playList[index].author;
+        let currentSong = playList[index].song;
+        let num = index + 1;
+        let songLi = document.createElement('li');
+        songLi.className = 'item-songs'
+        songLi.innerHTML = `${num}: ${currentAuthor} - ${currentSong}`;
+        songUl.append(songLi);
+    }
+
+
+let semafore = [
+    {
+    name: "red",
+    foneColor: "hsl(0, 100%, 50%)",
+    strokeColor: "hsl(0, 100%, 80%)",
+    },
+
+    {
+    name: "yellow",
+    foneColor: "hsl(60, 100%, 50%)",
+    strokeColor: "hsl(60, 100%, 80%)",
+    },
+
+    {
+    name: "green",
+    foneColor: "hsl(90, 100%, 50%)",
+    strokeColor: "hsl(90, 100%, 80%)",
+    },
+
+    {
+    name: "neutral",
+    foneColor: "hsl(0, 0%, 80%)",
+    strokeColor: "hsl(0, 0%, 70%)",
+    },
+]
+
+let boxSemafor = document.createElement('div');
+    boxSemafor.className = 'box-semafor';
+    document.body.appendChild(boxSemafor);
+
+    for (let index = 0; index < 3; ++index) {
+        let currentSection = document.createElement('div');
+        let sectionName = semafore[index].name;
+        currentSection.id = `${sectionName}`;
+        currentSection.className = 'section';
+        boxSemafor.appendChild(currentSection);
+    }
+
+let btnState = document.createElement('button');
+    btnState.className = 'btn-semafor';
+    btnState.innerHTML = 'Change State';
+    boxSemafor.after(btnState);
+
+function resetColor() {
+    for (let index = 0; index < 3; ++index) {
+        document.getElementById(`${semafore[index].name}`).style.backgroundColor = `${semafore[3].foneColor}`;
+        document.getElementById(`${semafore[index].name}`).style.borderColor = `${semafore[3].strokeColor}`;
+    }
+}
+
+let i = 0;
+    btnState.onclick = function() {
+        resetColor();
+        document.getElementById(`${semafore[i].name}`).style.backgroundColor = `${semafore[i].foneColor}`;
+        document.getElementById(`${semafore[i].name}`).style.borderColor = `${semafore[i].strokeColor}`;
+        i++;
+        if (i > 2) { i = 0};
+    }
+

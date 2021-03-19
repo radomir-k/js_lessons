aff149090d540718ae6860c1d389371b9624bb24
radomir-k <radomir.kartas@gmail.com>
2021-03-19 18:30:17 +0200

    lesson_33 v.01 begin

--- lesson_33/js/33.js
+++ lesson_33/js/33.js
@@ -8,7 +8,7 @@ let currentMap = L.map('usermap').setView(startCoordinates, startScale);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 22,
-    id: 'mapbox/streets-v9',
+    id: 'mapbox/streets-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: ownerToken,
